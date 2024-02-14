200bfa19c1777ce2a2845789066203cda065b2da
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-02-14 22:00:08 +0000

    Avoid hardcoded window values

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
+constexpr int SCREEN_HEIGHT = 640;
+constexpr int SCREEN_WIDTH = 480;
+
 int main(int argv, char **args) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    SDL_Window *window = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == nullptr) {
         std::cout << "Failed to create window! SDL_Error: " << SDL_GetError() << std::endl;
         return 1;
