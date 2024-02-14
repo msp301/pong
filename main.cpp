9465794bbc44c13dbeec6b8be71af6decb84d121
Martin Pritchard <martin@martinpritchard.com>
2024-02-14 20:41:57 +0000

    SDL2 Proof of Concept
    Create a window to prove one can be created.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+int main() {
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+
+    std::cout << "Hello, World!" << std::endl;
+
+    while (1) {
+    }
+
+    return 0;
+}
