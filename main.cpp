b407008b3061acf18474e23926796809620cef76
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-02-14 22:23:03 +0000

    Detect key pressed

--- main.cpp
+++ main.cpp
@@ -26,6 +26,9 @@ int main(int argv, char **args) {
     while (quit == false) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) quit = true;
+            if (e.type == SDL_KEYDOWN) {
+                std::cout << "Key " << e.key.keysym.scancode << " pressed" << std::endl;
+            }
         }
     }
 
