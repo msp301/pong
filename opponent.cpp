4edf5fd9e7c77355daed1e40d40e64b79504e48c
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-02-20 15:42:12 +0000

    Add variation to opponent movements
    Start to make it appear that the opponent is making mistakes

--- opponent.cpp
+++ opponent.cpp
@@ -1,10 +1,17 @@
+#include <random>
+
 #include "opponent.h"
 
 void Opponent::performMove(const int offset, const Position* ballPosition, const Position* previousBallPosition) {
     if (previousBallPosition == nullptr) return;
     if (ballPosition->Y > previousBallPosition->Y) return;
 
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(1, offset);
+    const int chosenOffset = distrib(gen);
+
     const int paddleCentre = position->X + (width / 2);
-    if (ballPosition->X > paddleCentre) return moveX(offset);
-    if (ballPosition->X < paddleCentre) return moveX(-offset);
+    if (ballPosition->X > paddleCentre) return moveX(chosenOffset);
+    if (ballPosition->X < paddleCentre) return moveX(-chosenOffset);
 }
